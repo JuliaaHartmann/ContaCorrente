97c5dccd8ef806e31d7a90359272dc234d941534
Júlia Hartmann <juliahartmann@outlook.com.br>
2026-04-07 10:55:47 -0300

    Adicionadas as operações de saque, depósito, transferência e consulta de saldo na classe ContaCorrente

--- ContaCorrente.ConsoleApp/Program.cs
+++ ContaCorrente.ConsoleApp/Program.cs
@@ -9,58 +9,134 @@
     Saque: Permite a retirada de valores, respeitando o limite máximo permitido.
     Depósito: Possibilita a adição de fundos à conta.
     Consulta de saldo: Fornece informações atualizadas sobre o montante disponível.
+    Transferência entre contas: Permite a movimentação de valores entre contas correntes distintas.
 */
 
 using System.Security.Cryptography;
 
-//Conta Corrente
-int numeroIdentificacao = RandomNumberGenerator.GetInt32(1, 101);
-string titular = "Julia";
-decimal saldo = 1000;
-decimal limiteDebito = 1200;
-
-while (true)
+class ContaCorrente
 {
-    Console.Clear();
-    Console.WriteLine("============================");
-    Console.WriteLine($"Conta Corrente de {titular}");
-    Console.WriteLine("============================");
-    Console.WriteLine("1 - Saque");
-    Console.WriteLine("2 - Depósito");
-    Console.WriteLine("3 - Consulta de Saldo");
-    Console.WriteLine("4 - Sair");
-    string? opcaoMenu = Console.ReadLine()?.ToUpper();
+    public int numeroIdentificacao;
+    public string titular;
+    public decimal saldo;
+    public decimal limiteDebito;
 
-    if (opcaoMenu == "S")
-        break;
-
-    if (opcaoMenu == "1")
+    public void Sacar()
     {
         Console.Write("Digite o valor que deseja sacar (R$): ");
         decimal valorSaque = Convert.ToDecimal(Console.ReadLine());
 
         if (saldo <= -limiteDebito)
-        {
-            Console.WriteLine("O valor do limite de débito já foi ultrapassado");
-            Console.ReadLine();
-        }
-        else
-        {
-            saldo -= valorSaque;
-            Console.WriteLine("O valor foi sacado com sucesso!");
-            Console.ReadLine();
-        }
+            if (valorSaque > saldo + limiteDebito)
+            {
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine("O valor do limite de débito já foi ultrapassado");
+                Console.ReadLine();
+            }
+            else
+            {
+                saldo -= valorSaque;
+                Console.WriteLine("O valor foi sacado com sucesso!");
+                Console.WriteLine("-------------------------------------");
+                Console.Write("Digite ENTER para continuar...");
+                Console.ReadLine();
+
+            }
+
+        Console.WriteLine("-------------------------------------");
+        Console.Write("Digite ENTER para continuar...");
+        Console.ReadLine();
     }
-    else if (opcaoMenu == "2")
+
+
+    public void Depositar()
     {
+        Console.WriteLine("-------------------------------------");
         Console.Write("Digite o valor que deseja depositar ()R$: ");
         decimal valorDeposito = Convert.ToDecimal(Console.ReadLine());
 
         saldo += valorDeposito;
-    }
-    else if (opcaoMenu == "3")
-    {
-        Console.WriteLine($"O valor do saldo da conta é (R$): {saldo}");
+
+        Console.WriteLine("-------------------------------------");
+        Console.WriteLine("O valor foi depositado com sucesso!");
+        Console.WriteLine("-------------------------------------");
+        Console.Write("Digite ENTER para continuar...");
         Console.ReadLine();
+    }
+
+    public void TransferirPara(ContaCorrente contaDestino)
+    {
+        Console.WriteLine("-------------------------------------");
+        Console.Write("Digite o valor que deseja transferir (R$): ");
+        decimal valorTransferencia = Convert.ToDecimal(Console.ReadLine());
+
+        saldo -= valorTransferencia;
+        contaDestino.saldo += valorTransferencia;
+
+        Console.WriteLine("-------------------------------------");
+        Console.WriteLine($"O valor dde R${valorTransferencia} foi transferido com sucesso!");
+        Console.WriteLine("-------------------------------------");
+        Console.Write("Digite ENTER para continuar...");
+        Console.ReadLine();
+    }
+
+    public void ObterSaldo()
+    {
+        Console.WriteLine("-------------------------------------");
+        Console.WriteLine($"O valor do saldo da conta é (R$): {saldo}");
+        Console.WriteLine("-------------------------------------");
+        Console.Write("Digite ENTER para continuar...");
+        Console.ReadLine();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Conta Corrente 1
+        ContaCorrente contaUm = new ContaCorrente();
+        contaUm.numeroIdentificacao = 1;
+        contaUm.titular = "Julia";
+
+        //Conta Corrente 2
+        ContaCorrente contaDois = new ContaCorrente();
+        contaUm.numeroIdentificacao = 2;
+        contaDois.saldo = 12000;
+        contaDois.titular = "Tiago";
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("============================");
+            Console.WriteLine($"Conta Corrente #{contaUm.numeroIdentificacao} de {contaUm.titular}");
+            Console.WriteLine("============================");
+            Console.WriteLine("1 - Saque");
+            Console.WriteLine("2 - Depósito");
+            Console.WriteLine("3 - Transferência");
+            Console.WriteLine("4 - Consulta de Saldo");
+            Console.WriteLine("5 - Sair");
+            string? opcaoMenu = Console.ReadLine()?.ToUpper();
+
+            if (opcaoMenu == "S")
+                break;
+
+            if (opcaoMenu == "1")
+            {
+                contaUm.Sacar();
+            }
+            else if (opcaoMenu == "2")
+            {
+                contaUm.Depositar();
+            }
+            else if (opcaoMenu == "3")
+            {
+                contaUm.TransferirPara(contaDois);
+            }
+            else if (opcaoMenu == "4")
+            {
+                contaUm.ObterSaldo();
+            }
+        }
     }
 }
